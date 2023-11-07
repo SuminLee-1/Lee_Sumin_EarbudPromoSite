04ac04a5ff5f48fc9ea4acd17010075e001d894e
“SuminLee-1” <sumin.lee707@gmail.com>
2023-11-07 09:31:39 -0500

    fixed some error between js file"

---
+++ js/burgercon.js
@@ -0,0 +1,18 @@
+
+(function(){
+
+	let button = document.querySelector("#burger");
+	let burgerCon = document.querySelector("#globalnav-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	// let hamburgerMenu = () => {
+	// 	burgerCon.classList.toggle("slide-toggle");
+	// 	button.classList.toggle("expanded");
+	// };
+
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
